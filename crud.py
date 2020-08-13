a6a4ce7eab2b2a92902e24b93a898f5cc9283264
Marissa Aguilera <marissaguilera@yahoo.com>
2020-08-12 17:31:44 -0700

    Get_user_by in crud operations

--- crud.py
+++ crud.py
@@ -3,6 +3,8 @@
 from model import db, User, Park, Activity, Bucketlist, BucketlistItem, connect_to_db
 #importing classes
 
+
+#Users
 def create_user(username, fname, lname, email, password):
     """Create and return a new user."""
 
@@ -17,11 +19,29 @@ def create_user(username, fname, lname, email, password):
 
     return user
 
+
 def get_user():
     """Return all users."""
-    
+
     return User.query.all()
 
+
+def get_user_by_id(user_id):
+    """Return a user by their ID/primary_key."""
+
+    return User.query.get(user_id)
+
+
+def get_user_by_email(email):
+    """Return a user by their email."""
+
+    return User.query.filter(User.email == email).first()
+
+
+def get_user_by_username(username):
+    """Return a user by their username."""
+
+    return User.query.filter(User.username == username).first()
 
 
 
