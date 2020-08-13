5a6c5554a00a95d6db239f47328626a8a1c4d818
Marissa Aguilera <marissaguilera@yahoo.com>
2020-08-12 17:43:22 -0700

    Create_park, get_parks, get_park_by_id in crud operations

--- crud.py
+++ crud.py
@@ -1,10 +1,10 @@
 """CRUD (create, read, update, delete) operations."""
 
+#importing classes from model.py
 from model import db, User, Park, Activity, Bucketlist, BucketlistItem, connect_to_db
-#importing classes
 
 
-#Users
+#User
 def create_user(username, fname, lname, email, password):
     """Create and return a new user."""
 
@@ -20,14 +20,14 @@ def create_user(username, fname, lname, email, password):
     return user
 
 
-def get_user():
+def get_users():
     """Return all users."""
 
     return User.query.all()
 
 
 def get_user_by_id(user_id):
-    """Return a user by their ID/primary_key."""
+    """Return a user by primary_key."""
 
     return User.query.get(user_id)
 
@@ -43,6 +43,34 @@ def get_user_by_username(username):
 
     return User.query.filter(User.username == username).first()
 
+
+#Park 
+def create_park(park_name, state_code, designation, imageURL):
+    """Create and return a new park."""
+
+    park = Park(park_name=park_name, 
+                state_code=state_code, 
+                designation=designation, 
+                imageURL=imageURL)
+    
+    db.session.add(park)
+    db.session.commit()
+
+    return park
+
+
+def get_parks():
+    """Return all parks."""
+
+    # Park.query.all()
+
+    return Park.query.filter(Park.designation == 'national park') #test this!
+
+
+def get_park_by_id(park_id):
+    """Return a park by primary key."""
+
+    return Park.query.get(park_id)
 
 
 
